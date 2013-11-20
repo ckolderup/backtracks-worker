d903e5f55a0d20c070aed780327fb5049da27b83
Casey Kolderup <casey@kolderup.org>
2013-11-19 20:07:07 -0500

    handle truncation in CSS

--- models/track.rb
+++ models/track.rb
@@ -5,9 +5,6 @@ class Track
 
   def initialize(param = {})
     @title = param[:title]
-    if @title.length > 31 then
-      @title = @title[0...28] + '...'
-    end
     @artist = param[:artist]
     @url = param[:url]
     @url = "http://#{@url}" unless @url.start_with?("http://")
